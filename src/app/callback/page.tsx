10b450b525c47584c92c9f0919e2086c38c02b77
naol86 <naol.kasinet@gmail.com>
2024-12-08 16:29:27 +0300

    deploy fixed

--- src/app/callback/page.tsx
+++ src/app/callback/page.tsx
@@ -1,45 +1,11 @@
 "use client";
+import { Suspense } from "react";
+import Callback from "./Callback";
 
-import { useEffect } from "react";
-import { useSearchParams, useRouter } from "next/navigation"; // Import router for client-side navigation
-import { signIn } from "next-auth/react";
-
-const Callback = () => {
-  const searchParams = useSearchParams();
-  const router = useRouter();
-
-  useEffect(() => {
-    const token = searchParams.get("token");
-
-    const login = async (token: string) => {
-      try {
-        const result = await signIn("google-credentials", {
-          token,
-          redirect: false,
-        });
-        if (result?.ok) {
-          // Successful sign-in
-          router.push("/");
-        } else {
-          // Handle sign-in errors
-          console.error("Sign-in error:", result?.error);
-          router.push("/error"); // Redirect to an error page
-        }
-      } catch (err) {
-        console.error("Unexpected error during sign-in:", err);
-        router.push("/error"); // Redirect to an error page
-      }
-    };
-
-    if (token) {
-      login(token);
-    } else {
-      console.error("Token missing in query parameters");
-      router.push("/error"); // Redirect to an error page
-    }
-  }, [searchParams, router]);
-
-  return <p>Loading...</p>;
-};
-
-export default Callback;
+export default function page() {
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <Callback />
+    </Suspense>
+  );
+}
