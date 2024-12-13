cdc54212ff5aed51d64cddaa04565ff32167ebcb
naol86 <naol.kasinet@a2sv.org>
2024-12-13 20:17:18 +0300

    change favicon and add some meta data

--- src/app/ai/layout.tsx
+++ src/app/ai/layout.tsx
@@ -1,7 +1,14 @@
 import AppSidebar from "@/components/Sidebar/AppSidebar";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import CustomSessionProvider from "@/hooks/CustomeSessionProvider";
+import { Metadata } from "next";
 import React from "react";
+
+export const metadata: Metadata = {
+  title: "Chat",
+  description:
+    "Naol AI is always happy to help! Engage in dynamic conversations, get insightful responses, and explore ideas effortlessly with our interactive web app.",
+};
 
 export default function layout({ children }: { children: React.ReactNode }) {
   return (
